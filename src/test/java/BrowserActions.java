c3f4f33ad3c3fd8c0e4f247e72a223d6656a9965
Sbenii <sezeranben@gmail.com>
2025-09-25 18:07:57 +0200

    Add getCurrentUrl, getTitle and getWindowHandle functionalities

--- src/test/java/BrowserActions.java
+++ src/test/java/BrowserActions.java
@@ -1,6 +1,7 @@
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
@@ -12,15 +13,23 @@ public class BrowserActions {
         driver = new ChromeDriver();
         driver.get("https://www.google.com");
         navigation("https:www.amazon.com");
-//        maximize();
+        maximize();
 //        navigateBack();
 //        navigateForward();
-//        refresh();
+        refresh();
 //        minimize();
 //        setPosition();
-        IpadDimension();
-        IphoneDimension();
+//        fullscreen();
+//        IpadDimension();
+//        IphoneDimension();
+        getUrl();
+        getTitle();
+//        getPageSource();
+        getWindowHandle();
+        driver.switchTo().newWindow(WindowType.TAB);
+        getWindowHandle();
     }
+
 
     public void navigation(String url) {
         driver.navigate().to(url);
@@ -42,6 +51,10 @@ public class BrowserActions {
         driver.manage().window().maximize();
     }
 
+    public void fullscreen() {
+        driver.manage().window().fullscreen();
+    }
+
     public void minimize() {
         driver.manage().window().minimize();
     }
@@ -59,5 +72,25 @@ public class BrowserActions {
     public void IphoneDimension() {
         Dimension dimension = new Dimension(390, 844);
         driver.manage().window().setSize(dimension);
+    }
+
+    public void getUrl() {
+        String url = driver.getCurrentUrl();
+        System.out.println("Current url: " + url);
+    }
+
+    public void getTitle() {
+        String title = driver.getTitle();
+        System.out.println("The title: " + title);
+    }
+
+    public void getPageSource() {
+        String source = driver.getPageSource();
+        System.out.println("Page source: " + source);
+    }
+
+    public void getWindowHandle() {
+        String windowHandle = driver.getWindowHandle();
+        System.out.println("The window handle: " + windowHandle);
     }
 }
