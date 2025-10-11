429c7323ebcf81f9abfeb579d89863cf248918f6
Sbenii <sezeranben@gmail.com>
2025-10-11 16:15:08 +0200

    Add Open browser test for Auca

--- src/test/java/Auca.java
+++ src/test/java/Auca.java
@@ -1,8 +1,25 @@
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 public class Auca {
     WebDriver driver;
-    @BeforeTest
 
+    @BeforeTest
+    public void prepare() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterTest
+    public void end() throws InterruptedException {
+        Thread.sleep(3000);
+        driver.quit();
+    }
+
+    @Test(priority = 0)
+    public void OpenBrowser() {
+        driver.navigate().to("https://registration.auca.ac.rw/");
+    }
 }
