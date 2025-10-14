e880e86a7586f5ba05a2fe57ee9cbe6bc27184f7
Sbenii <sezeranben@gmail.com>
2025-10-14 22:34:19 +0200

    Add fing flight tests

--- src/test/java/BlazeDemo.java
+++ src/test/java/BlazeDemo.java
@@ -1,6 +1,8 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -28,5 +30,20 @@ public class BlazeDemo {
     @Test(priority = 1)
     public void CheckDestinationOfTheWeek() {
         driver.findElement(By.partialLinkText("destination of the week! The Beach!")).click();
+    }
+
+    @Test(priority = 2)
+    public void FindFlight() {
+        WebElement DepartureCity = driver.findElement(By.name("fromPort"));
+        WebElement DestinationCity = driver.findElement(By.name("toPort"));
+        Select selectDeparture = new Select(DepartureCity);
+        Select selectDestination = new Select(DestinationCity);
+        DepartureCity.click();
+        selectDeparture.selectByValue("São Paolo");
+        DepartureCity.click();
+        DestinationCity.click();
+        selectDestination.selectByValue("Cairo");
+        DestinationCity.click();
+        driver.findElement(By.xpath("/html/body/div[3]/form/div/input")).click();
     }
 }
